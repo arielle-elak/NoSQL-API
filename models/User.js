1f9d41149dd1228818abfb555436b50f5711de76
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-30 13:55:57 -0700

    User model include vitruals and ref external arrays

--- models/User.js
+++ models/User.js
@@ -3,33 +3,48 @@
 const { Schema, Types } = require('mongoose');
 
 const userSchema = new Schema(
-    {
-        userId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-            // Regex from https://gist.github.com/rupeshtiwari/acf770bfc85f3fe1f62a80b461abfc13
-            match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email address.'],
-        },
-        thoughts: [
-
-        ]
-
-        },
-        friends: [
-
-        ],
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      // Regex from https://gist.github.com/rupeshtiwari/acf770bfc85f3fe1f62a80b461abfc13
+      match: [
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email address.",
+      ],
+    },
+    // Array of thoughts from Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    // Array of friends (users) from User model
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 // Virtual to retrieve the length of user's 'friends' array
