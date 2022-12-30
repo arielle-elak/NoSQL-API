078e909f8f531eeeb148ae2d3e743e28cb777eaa
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-30 14:46:47 -0700

    Root index for api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,9 @@
-// Basic index for the api routes
-const router = require("express").Router();
+// Root Index for API Routes
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/users', thoughtRoutes);
+router.use('/thoughts', userRoutes);
 
 module.exports = router;
