4f530d8e925d73a699a9c697c794f1a262bdab29
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-30 14:41:52 -0700

    No virtuals for reaction, so normal export6s

--- models/Thought.js
+++ models/Thought.js
@@ -3,40 +3,39 @@
 const { Schema, Types } = require('mongoose');
 
 const dateFormat = require('../utils/dateFormat');
+const reactionSchema = require("./Reaction");
 
 const thoughtSchema = new Schema(
-    {
-        thoughtId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            // Format timestamp using moment.js
-            get: timestamp => dateFormat(timestamp)
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        // Array of reactions from the Reaction model
-        reactions: // TODO
-            [],
+  {
+    thoughtId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-        toJSON: {
-            getters: true,
-            virtuals: true,
-        },
-        id: false,
-    }
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Format timestamp using moment.js
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 thoughtSchema.virtual('reactionCount').get(function() {
