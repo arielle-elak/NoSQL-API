9d92a4e68d5f8d0b8794bc16f36de805c04d1008
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-30 14:55:25 -0700

    User routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,16 @@
 // User API Routes for api/users/
+
+const router = require("express").Router();
+
+const { getAllUsers, getOneUser, createUser } = require("../../controllers");
+// TODO: Set up controller paths
+
+// ROOT /api/users - GET all users and POST a new user
+router.route("/")
+    .get(getAllUsers)
+    .post(createUser);
+
+// USERID /api/users/:userId - GET one user
+router.route("/:userId").get(getOneUser);
+
+module.exports = router;
