409eb9b5531358ba82eb6e7e73f4149284a8ed3d
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-30 13:47:39 -0700

    Both thoughts and friends are arrays

--- models/User.js
+++ models/User.js
@@ -9,18 +9,26 @@ const userSchema = new Schema(
             default: () => new Types.ObjectId(),
         },
         username: {
-
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
         },
         email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: //TODO,
+        },
+        thoughts: [
+
+        ]
 
         },
-        thoughts: {
+        friends: [
 
-        },
-        friends: {
-
-        },
-    }
+        ],
+    },
 );
 
 // Virtual to retrieve the length of user's 'friends' array
