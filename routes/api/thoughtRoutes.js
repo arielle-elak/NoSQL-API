53cd9d332f2da653cf94fcb0fd5b20bc7bb23202
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-30 15:10:51 -0700

    All thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,13 +6,29 @@ const {
   getAllThoughts,
   getOneThought,
   createThought,
+  updateThought,
   deleteThought,
+  addReaction,
+  deleteReaction,
 } = require("../../controllers");
 // TODO: Set up controller paths
 
 // ROOT /api/thoughts - GET all thoughts and and POST a new thought
-router.route('/')
+router.route("/")
     .get(getAllThoughts)
     .post(createThought);
 
+// THOUGHTID /api/thoughts/:thoughtId - GET one thoguht, PUT thought updates, DELETE thought
+router
+  .route("/:thoughtId")
+  .get(getOneThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
+// REACTIONS /api/thoughts/:thoughtId/reactions - POST add reaction to react array
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// REACTIONS /api/thoughts/:thoughtId/reactions/:reactionId - DELETE remove one reaction from array
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
