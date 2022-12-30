52a94d94e2762a220bd4fc5323812798a7ef8496
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-30 15:02:33 -0700

    setting up thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,13 @@
 // Thought API Routes for api/thoughts/
 
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getOneThought,
+  createThought,
+  deleteThought,
+} = require("../../controllers");
+// TODO: Set up controller paths
+
 
